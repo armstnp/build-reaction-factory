bdc3b7c12049869924cd81add1105983b9e1a000
Nathan Armstrong <nathan.armstrong@parivedasolutions.com>
2014-06-07 22:21:00 -0700

    Add a static list of stock reactions to display alongside the generated image.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,4 +20,18 @@ app.config(function ($routeProvider) {
   //TODO: figure out proper environment injection for the app!
   .constant('SUCCESS_ALBUM_ID', '')
   .constant('FAILURE_ALBUM_ID', '')
-  .constant('IMGUR_CLIENT_ID', '');
+  .constant('IMGUR_CLIENT_ID', '')
+  .constant('SUCCESS_REACTIONS', [
+	'Nice build!',
+	':)',
+	'Impressive.',
+	'Hey, you pulled it off!',
+	'Still as stable as stone.'
+  ])
+  .constant('FAILURE_REACTIONS', [
+	':(',
+	"That's too bad...",
+	'you tried',
+	'There, there; you can always push a fix.',
+	"Yikes, didn't expect that!"
+  ]);
